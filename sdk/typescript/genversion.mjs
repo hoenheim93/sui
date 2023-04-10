a719275ad2c7ab2bd8cd782813837ee6028939bb
Jordan Gensler <jordan@mystenlabs.com>
2023-04-10 15:01:00 -0700

    Make target RPC version generated

--- sdk/typescript/genversion.mjs
+++ sdk/typescript/genversion.mjs
@@ -2,15 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { readFile, writeFile } from 'fs/promises';
+import TOML from '@iarna/toml';
 
 const LICENSE =
   '// Copyright (c) Mysten Labs, Inc.\n// SPDX-License-Identifier: Apache-2.0\n\n';
 
 async function main() {
   const pkg = JSON.parse(await readFile('./package.json', 'utf8'));
+  const cargo = TOML.parse(await readFile('../../Cargo.toml', 'utf8'));
   await writeFile(
     'src/pkg-version.ts',
-    LICENSE + `export const pkgVersion = '${pkg.version}';\n`,
+    LICENSE +
+      `export const PACKAGE_VERSION = '${pkg.version}';\nexport const TARGETED_RPC_VERSION = '${cargo.workspace.package.version}';\n`,
   );
 }
 
--- sdk/typescript/src/pkg-version.ts
+++ sdk/typescript/src/pkg-version.ts
@@ -1,4 +1,5 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-export const pkgVersion = '0.31.0';
+export const PACKAGE_VERSION = '0.31.0';
+export const TARGETED_RPC_VERSION = '0.32.0';
